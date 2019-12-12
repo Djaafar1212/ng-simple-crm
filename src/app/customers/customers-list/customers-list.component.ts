ec3073455e9a5058901a45228e1b6f17342d6bf1
Djaafar12 <dmechekvil@educacion.navarra.es>
2019-12-12 10:02:05 +0100

    first commit

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -14,12 +14,28 @@ export class CustomersListComponent implements OnInit {
     constructor() { }
 
     ngOnInit() {
+      this.filteredCustomers = [
+        {
+          id: 1, name: "Ion Egizu", city: "Elizondo", orderTotal: 9.99, customerSince: new Date(2014, 7, 10)
+        },
+        {
+          id: 2, name: "Ane Doe", city: "Pamplona", orderTotal: 19.99, customerSince: new Date(2017, 2, 22)
+        },
+        {
+          id: 3, name: "Juan sin tierra", city: "Tudela", orderTotal: 99.99, customerSince: new Date(2019, 10, 13)
+        },
+        {
+          id: 4, name: "Julia Thomas", city: "Tafalla", orderTotal: 599.99, customerSince: new Date(2012, 10, 31)
+        }
+      ];
+      this.calculateOrders();
     }
 
     calculateOrders() {
       this.customersOrderTotal = 0;
       this.filteredCustomers.forEach( (cust:ICustomer) => {
         this.customersOrderTotal += cust.orderTotal;
+        console.log('suma: ')
       } );
     }
 }
